e4b6b9b28914cd1af59a7137003753d7388ed482
Daydreamer <786457306@qq,com>
2019-12-10 20:57:25 +0800

    添加eureka client模块

---
+++ micro-weather-eureka-client/src/main/java/com/zzw/weather_eureka_client/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.zzw.weather_eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Daydreamer
+ * @date 2019/12/10 20:49
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
